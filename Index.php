8246c80887ce91d00b8c0571b2114c45299b4212
MuhammadBayuPratama <bayucaem2000@gmail.com>
2024-05-19 20:25:51 +0700

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,28 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AplikasiPendaftaran</title>
+</head>
+<body>
+    <form action="register.php" method="post">
+        <div>
+            <label for="">ID</label>
+            <input type="text" name="ID">
+        </div>
+        <div>
+            <label for="">Nama</label>
+            <input type="text" name="Nama">
+        </div>
+        <div>
+            <label for="">Email</label>
+            <input type="Email" name="Email">
+        </div>
+        <div>
+            <label for="">Password</label>
+            <input type="Password" name="Password">
+        </div>
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
